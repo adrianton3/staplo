36a3257d2c7333c9311777b77fa80c1aebb3c331
Adrian Toncean <adrianton3@gmail.com>
2015-05-02 22:16:06 +0200

    Harder levels work; refactored a bit

--- src/staplo/generator.cljs
+++ src/staplo/generator.cljs
@@ -2,11 +2,24 @@
   (:require
     [staplo.operations :as operations]))
 
-(defn generate-string [length chars]
+(defn rand2 [start end]
+  (+ start (rand-int (- end start))))
+
+(defn rand-interval [{:keys [start end]}]
+  (rand2 start end))
+
+; clojure probably has this already
+(defn accumulate [fun base times]
   (reduce
+    #(fun %)
+    base
+    (range 0 times)))
+
+(defn generate-string [length chars]
+  (accumulate
     #(str % (rand-nth chars))
     ""
-    (range 0 length)))
+    length))
 
 (defn generate-ops [start-text steps operations]
   (letfn [
@@ -14,15 +27,14 @@
       (let [op-name (rand-nth operations)
             op (get operations/operations op-name)]
         (op text)))]
-    (reduce
-      step
-      start-text
-      (range 0 steps))))
+    (accumulate step start-text steps)))
 
 (defn generate-challenge [config]
   (let [char-sets [["a" "b"] ["a" "b" "c"]]
         char-set (rand-nth char-sets)
-        start (generate-string 4 char-set)
-        target (generate-ops start 3 (:operations config))]
+        start-length (rand-interval (:start-length config))
+        start (generate-string start-length char-set)
+        steps (rand-interval (:steps config))
+        target (generate-ops start steps (:operations config))]
     {:start start
      :target target}))
--- src/staplo/levels.cljs
+++ src/staplo/levels.cljs
@@ -13,4 +13,20 @@
     :name "medium"
     :start-length (interval 4 5)
     :operations ["reverse" "rotate" "duplicate-last" "unwrap"]
+    :steps (interval 4 5)}
+  {
+    :name "hard"
+    :start-length (interval 4 5)
+    :operations ["reverse" "rotate" "unwrap"
+                 "push-a"
+                 "push-b"
+                 "push-c"]
+    :steps (interval 4 5)}
+  {
+    :name "harder"
+    :start-length (interval 5 6)
+    :operations ["reverse" "rotate" "duplicate-last" "pop"
+                 "ab -> c"
+                 "bc -> a"
+                 "ca -> b"]
     :steps (interval 4 5)}])
