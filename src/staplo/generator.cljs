8848ce2d71614bb34e3c7533dad126ff82068c2c
Adrian Toncean <adrianton3@gmail.com>
2015-05-10 14:06:42 +0200

    Avoiding cycles

--- src/staplo/generator.cljs
+++ src/staplo/generator.cljs
@@ -40,16 +40,18 @@
      generate-string
      generate-number) length))
 
-; too many parameters
-(defn generate-op [operations operation-names text history]
+; way too many parameters
+(defn generate-op [operations operation-names start-text text history]
   (let [rand-op
         #(let [op-name (rand-nth operation-names)]
             {:op-name op-name
              :op (operations op-name)})]
     (loop [candidate-op (rand-op)]
-      (if ((:precondition (:op candidate-op)) text history)
-        candidate-op
-        (recur (rand-op))))))
+      (let [{{precondition :precondition operation :operation} :op} candidate-op
+            result (operation text)]
+        (if (and (precondition text history) (not= start-text result))
+          candidate-op
+          (recur (rand-op)))))))
 
 (defn generate-ops [start-text steps operations-config]
   (letfn [
@@ -58,6 +60,7 @@
             {op-name :op-name op :op} (generate-op
                                         (operations/operations op-sort)
                                         (:list operations-config)
+                                        start-text
                                         text
                                         history)]
         {:text ((:operation op) text)
