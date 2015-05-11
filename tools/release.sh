8f2a5011b77e412c28e60205062ec148bf20a550
Adrian Toncean <adrianton3@gmail.com>
2015-05-11 10:30:31 +0200

    Release script

---
+++ tools/release.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+mkdir tmp
+cp out tmp -r
+git checkout gh-pages
+cp tmp/out ./ -r
+
+git checkout master -- index.html
+git checkout master -- game.html
+git checkout master -- style.html
+git checkout master -- lib
